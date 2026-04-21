f44d122948c25d4b47efcab6ebafbff185ca9f4a
ines-hernandez-de-azcoitia <ines.hernandez.de.azcoitia@estudiantat.upc.edu>
2026-04-21 11:57:02 +0200

    Version 2

---
+++ LEBL.py
@@ -0,0 +1,20 @@
+def SetGates (area, init_gate, end_gate, prefix):
+    return
+
+def LoadAirlines (terminal, t_name):
+    return
+
+def LoadAirportStructure (filename):
+    return
+
+def GateOccupancy (bcn):
+    return
+
+def IsAirlineInTerminal (terminal, name):
+    return
+
+def SearchTerminal (bcn, name):
+    return
+
+def AssignGate (bcn, aircraft):
+    return
